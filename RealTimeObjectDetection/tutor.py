84f5baf36c7803c17a1d376fcfebc569e2e6b457
aishareog <aishao18@ru.is>
2022-12-01 12:59:17 +0000

    Adding implemented mediapipe trained network

--- RealTimeObjectDetection/tutor.py
+++ RealTimeObjectDetection/tutor.py
@@ -7,19 +7,25 @@ import time
 
 IMAGE_PATH = 'Tensorflow\\workspace\\images\\colectedimages'
 labels = ['LEFT','RIGHT', 'UP', 'DOWN', 'NADA']
-number_imgs = 20
+number_imgs = 10
+counter = 0
 
 for lable in labels:
     os.mkdir('Tensorflow\\workspace\\images\\colectedimages\\'+ lable)
-    cap = cv2.VideoCapture(1)
+    cap = cv2.VideoCapture(0)
     print('COLLECTING images for {}'.format(lable))
     time.sleep(5)
     for imgnum in range(number_imgs):
+        print(counter)
+        if counter == 10:
+            print('switch')
+            counter = 0
         ret,frame = cap.read()
         imagename = os.path.join(IMAGE_PATH, lable, lable + '_' +'{}.jpg'.format(str(uuid.uuid1())))
         cv2.imwrite(imagename,frame)
         cv2.imshow('frame',frame)
         time.sleep(2)
+        counter += 1
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
